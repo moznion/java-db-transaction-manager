8d65152439bdf3028cecbd362a6bb75b4f4332dd
moznion <moznion@gmail.com>
2015-05-13 00:22:40 +0900

    Format!

--- src/main/java/net/moznion/db/transaction/manager/TransactionScope.java
+++ src/main/java/net/moznion/db/transaction/manager/TransactionScope.java
@@ -5,74 +5,73 @@ import java.util.List;
 
 /**
  * The handler of a transaction manager which is scope (means try-with-resources statement) based.
- * 
- * @author moznion
  *
+ * @author moznion
  */
 public class TransactionScope implements AutoCloseable {
-	private boolean isActioned = false;
-	private final TransactionManager transactionManager;
+    private boolean isActioned = false;
+    private final TransactionManager transactionManager;
 
-	/**
-	 * Constructs a handler of a transaction manager with is scope based.
-	 * 
-	 * @throws SQLException
-	 */
-	public TransactionScope(TransactionManager transactionManager) throws SQLException {
-		this.transactionManager = transactionManager;
-		if (transactionManager == null) {
-			throw new IllegalArgumentException("transactionManager must not be null");
-		}
+    /**
+     * Constructs a handler of a transaction manager with is scope based.
+     *
+     * @throws SQLException
+     */
+    public TransactionScope(TransactionManager transactionManager) throws SQLException {
+        this.transactionManager = transactionManager;
+        if (transactionManager == null) {
+            throw new IllegalArgumentException("transactionManager must not be null");
+        }
 
-		Boolean originalAutoCommitStatus = transactionManager.getOriginalAutoCommitStatus();
-		if (originalAutoCommitStatus == null) {
-			originalAutoCommitStatus = transactionManager.getConnection().getAutoCommit();
-		}
-		transactionManager.txnBegin(originalAutoCommitStatus);
-	}
+        Boolean originalAutoCommitStatus = transactionManager.getOriginalAutoCommitStatus();
+        if (originalAutoCommitStatus == null) {
+            originalAutoCommitStatus = transactionManager.getConnection().getAutoCommit();
+        }
+        transactionManager.txnBegin(originalAutoCommitStatus);
+    }
 
-	/**
-	 * Commits the current transaction.
-	 * 
-	 * @throws SQLException
-	 */
-	public void commit() throws SQLException {
-		if (isActioned) {
-			return; // do not run twice
-		}
+    /**
+     * Commits the current transaction.
+     *
+     * @throws SQLException
+     */
+    public void commit() throws SQLException {
+        if (isActioned) {
+            return; // do not run twice
+        }
 
-		transactionManager.txnCommit();
-		isActioned = true;
-	}
+        transactionManager.txnCommit();
+        isActioned = true;
+    }
 
-	/**
-	 * Rollbacks the current transaction.
-	 * 
-	 * @throws SQLException
-	 */
-	public void rollback() throws SQLException {
-		if (isActioned) {
-			return; // do not run twice
-		}
+    /**
+     * Rollbacks the current transaction.
+     *
+     * @throws SQLException
+     */
+    public void rollback() throws SQLException {
+        if (isActioned) {
+            return; // do not run twice
+        }
 
-		transactionManager.txnRollback();
-		isActioned = true;
-	}
+        transactionManager.txnRollback();
+        isActioned = true;
+    }
 
-	@Override
-	public void close() throws SQLException {
-		List<TransactionTraceInfo> activeTransactions = transactionManager.getActiveTransactions();
-		if (activeTransactions.isEmpty()) {
-			return;
-		}
+    @Override
+    public void close() throws SQLException {
+        List<TransactionTraceInfo> activeTransactions = transactionManager.getActiveTransactions();
+        if (activeTransactions.isEmpty()) {
+            return;
+        }
 
-		TransactionTraceInfo currentTransactionTraceInfo = activeTransactions.get(activeTransactions.size() - 1);
-		if (Thread.currentThread().getId() != currentTransactionTraceInfo.getThreadId()) {
-			return;
-		}
+        TransactionTraceInfo currentTransactionTraceInfo = activeTransactions.get(activeTransactions.size() - 1);
+        if (Thread.currentThread().getId() != currentTransactionTraceInfo.getThreadId()) {
+            return;
+        }
 
-		if (!isActioned) {
-			rollback();
-		}
-	}
+        if (!isActioned) {
+            rollback();
+        }
+    }
 }
